ddf7d05160022a163e4f3a185ddd2907d7caff51
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2023-12-09 15:10:23 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import global from "../global.css";
+  </script>
+  <slot />
+
+<!-- <footer>
+    &#169; {new Date().getFullYear()} Stichting STRONG &#183; All Rights Reserved
+</footer> -->
+
