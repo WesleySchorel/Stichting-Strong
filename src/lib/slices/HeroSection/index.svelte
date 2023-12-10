03551610ebc82a273d679ecd8da30b9581e56a40
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2023-12-10 17:36:13 +0100

    Routing fix

--- src/lib/slices/HeroSection/index.svelte
+++ src/lib/slices/HeroSection/index.svelte
@@ -9,7 +9,7 @@
       <Header />
       <h1><span>{slice.primary.title}</span></h1>
       <p>{slice.primary.text}</p>
-      <a href="#/">Over de stichting →</a>
+      <a href="/">Over de stichting →</a>
     </section>
     
     <div class="img-container">
