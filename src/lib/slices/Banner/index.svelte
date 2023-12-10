9ba5aaef205ce1de83e96c319e4f79803a9690b1
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2023-12-10 17:23:58 +0100

    Object object fix
    Changelog:
    - added correct image to fix object object

--- src/lib/slices/Banner/index.svelte
+++ src/lib/slices/Banner/index.svelte
@@ -7,20 +7,24 @@
   <section>
     <h2>{slice.primary.title}</h2>
     <p>{slice.primary.text}</p>
-    <img src={slice.primary.image} alt={slice.primary.image} />
+    <img src={slice.primary.image.url} alt={slice.primary.title} />
   </section>
 </div>
 
 <style>
   div {
-    border: 3px solid red;
     padding: 8rem;
+    /* border: 3px solid red; */
   }
 
   section {
-    background-color: rgb(180, 230, 255);  
+    background-color: rgb(180, 230, 255);
     padding: 4rem;
     border-radius: 10px;
+  }
+
+  img {
+    width: 10%;
   }
 
   @media screen and (max-width: 480px) {
