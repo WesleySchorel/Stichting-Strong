8361801c0ae5cf0a55cbd140fd57f52bdc1a2eb5
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2023-12-28 21:55:41 +0100

    Fixed link
    Changelog:
    - Changed over-de-stichting to over-ons

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -24,7 +24,7 @@
             <li><a href="/meldkaart">Meldkaart</a></li>
             <li><a href="/education">Education</a></li>
             <li><a href="/nieuws">Nieuws</a></li>
-            <li><a href="/over-de-stichting">Over de stichting</a></li>
+            <li><a href="/over-ons">Over de stichting</a></li>
         </ul>
     </div>
     <div>
